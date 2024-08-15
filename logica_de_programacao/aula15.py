8ef6009e7c739a58773bc1167ff739c859d23f5d
Iago Carvalho <iagobgc@gmail.com>
2024-08-14 23:15:22 -0300

    Aulas 15 e 16

---
+++ logica_de_programacao/aula15.py
@@ -0,0 +1,10 @@
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome}')
+
+numero_um = input('Digite um numero: ')
+numero_dois = input('Digite o segundo numero: ')
+
+int_numero_um = int(numero_um) # Convertendo a str em int 
+int_numero_dois = int(numero_dois)
+
+print(f'A soma dos números é: {int_numero_um + int_numero_dois}')
