30eb336c489f1ffdf3716fb666b25fe36fdfc6b9
Iago Carvalho <iagobgc@gmail.com>
2024-08-14 22:49:19 -0300

    Terminei a aula

--- logica_de_programacao/aula14.py
+++ logica_de_programacao/aula14.py
@@ -1,6 +1,14 @@
+# Formatacao de strings com .format
 a = 'A'
 b = 'B'
 c = 1.1
-formato = ''
+string = 'a={} b={} c={}'
+string2 = 'a={0} b={1} c={2}' # Por indice
+string3 = 'a={nome1} b={nome2} c={nome3}' # Por parametro
+formato = string.format(a, b, c)
+formato2 = string2.format(a, b, c)
+formato3 = string3.format(nome1 = a, nome2 = b, nome3 = c)
 
 print(formato)
+print(formato2)
+print(formato3)
